51ebd011b4ec14c98c6e05d033848a16382d8b80
Geoffroy PLANQUART <geoffroy@home.aethelflaed.com>
2012-04-29 14:09:06 +0200

    Implemented trashcan static logic

--- src/Trashcan.cpp
+++ src/Trashcan.cpp
@@ -1,4 +1,5 @@
 #include "Trashcan.hpp"
+#include <algorithm>
 
 std::vector<Trashcan> Trashcan::trashcans;
 
@@ -7,6 +8,49 @@ std::vector<Trashcan> Trashcan::trashcans;
  */
 const Trashcan& Trashcan::getTrashcan(fs::path forFile, const User& user)
 {
+	forFile = fs::absolute(forFile);
+	if (isInHome(forFile, user))
+	{
+		return getTrashcanInDirectory(user.getHome(), user, false);
+	}
+	return getTrashcanInDirectory(cppmounts::for_path(forFile.string())->getPath(), user);
+}
+
+bool Trashcan::isInHome(const fs::path& file, const User& user)
+{
+	fs::path home{user.getHome()};
+	for (auto it = home.begin(), file_it = file.begin();
+		 it != home.end() && file_it != file.end();
+		 it++, file_it++)
+	{
+		if (*it != *file_it)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+Trashcan& Trashcan::getTrashcanInDirectory(fs::path directory_path, const User& user, bool fs_trash)
+{
+	fs::path path;
+	if (fs_trash)
+	{
+		path = directory_path / ".Trash";
+	}
+	else
+	{
+		path = directory_path / "Trash";
+	}
+
+	auto it = std::find(trashcans.begin(), trashcans.end(), path);
+	if (it == trashcans.end())
+	{
+		it = trashcans.begin();
+		it = trashcans.insert(it, Trashcan(directory_path, user, fs_trash));
+		std::cout << "new trashcan" << std::endl;
+	}
+	return *it;
 }
 
 /**
@@ -17,8 +61,23 @@ const Trashcan& Trashcan::getTrashcan(fs::path forFile, const User& user)
  * The optional parameter fs_trash control whether the trash directory
  * should be hidden or not.
  */
-Trashcan::Trashcan(fs::path path, bool fs_trash)
+Trashcan::Trashcan(fs::path path, const User& user, bool fs_trash)
+	:uid{user.getUID()},
+	 fs_trash{fs_trash}
 {
+	if (fs_trash)
+	{
+		this->path = path / ".Trash";
+	}
+	else
+	{
+		this->path = path / "Trash";
+	}
+}
+
+bool Trashcan::operator==(const fs::path& path)
+{
+	return this->path == path;
 }
 
 const fs::path& Trashcan::getPath() const noexcept
--- src/Trashcan.hpp
+++ src/Trashcan.hpp
@@ -2,6 +2,7 @@
 #define TRASH_TRASHCAN_HPP
 
 #include <boost/filesystem.hpp>
+#include <cppmounts.hpp>
 #include <vector>
 #include <string>
 #include "User.hpp"
@@ -17,13 +18,19 @@ public:
 
 	operator const fs::path&();
 	operator const std::string&();
+	bool operator==(const fs::path& path);
 
 private:
-	Trashcan(fs::path path, bool fs_trash = true);
+	static bool isInHome(const fs::path& file, const User& user);
+	static Trashcan& getTrashcanInDirectory(fs::path directory_path, const User& user, bool fs_trash = true);
+
+	Trashcan(fs::path path, const User& user, bool fs_trash = true);
 
 	static std::vector<Trashcan> trashcans;
 
 	fs::path path;
+	uid_t uid;
+	bool fs_trash;
 };
 
 #endif /* TRASH_TRASHCAN_HPP */
